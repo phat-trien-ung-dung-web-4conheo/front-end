8bb2cd8b4beb2869a643b129bd0c90b689e67118
HLOEY <70062580+HLoey258@users.noreply.github.com>
2022-11-15 16:50:14 +0700

    add Banner

--- src/components/Page/FullPage/FullPage.js
+++ src/components/Page/FullPage/FullPage.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../../Footer";
 import Header from "../../Header";
-
+import Banner from "../../Banner";
 const FullPage = () => {
   return (
     <>
       <Header></Header>
       <Outlet></Outlet>
+      <Banner></Banner>
       <Footer></Footer>
     </>
   );
