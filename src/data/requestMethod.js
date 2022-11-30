7e98c91b060b1ca9ba69077c4c42e7ca96f9ebe8
HLOEY <70062580+HLoey258@users.noreply.github.com>
2022-12-01 06:29:12 +0700

    .

--- src/data/requestMethod.js
+++ src/data/requestMethod.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api";
+const BASE_URL = "https://webdevis207.herokuapp.com/";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
