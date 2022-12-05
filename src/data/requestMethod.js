2e87571b9a6998b4dcf9ad5c666c9830df93cec3
Pau15122 <77733880+vandangnhathung@users.noreply.github.com>
2022-12-05 15:09:21 +0700

    change endpoint api

--- src/data/requestMethod.js
+++ src/data/requestMethod.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api/";
+const BASE_URL = "https://back-end-webdevis207.up.railway.app/api/";
 // const BASE_URL = "https://webdevis207.herokuapp.com/api";
 
 export const publicRequest = axios.create({
