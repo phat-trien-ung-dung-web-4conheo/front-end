9737374f6084d16acc60282b48456993e001ddd1
Pau15122 <77733880+vandangnhathung@users.noreply.github.com>
2023-05-19 02:13:23 +0700

    ok

--- src/data/requestMethod.js
+++ src/data/requestMethod.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// const BASE_URL = "https://back-end-webdevis207.up.railway.app/api/";
+const BASE_URL = "https://back-end-webdevis207.up.railway.app/api/";
 // const BASE_URL = "https://webdevis207.up.railway.app/api";
 // =======
-const BASE_URL = "http://localhost:3003/api/";
+// const BASE_URL = "http://localhost:3003/api/";
 // // const BASE_URL = "https://webdevis207.herokuapp.com/api";
 // >>>>>>> feature/user/login
 
